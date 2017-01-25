e6c8cd6544409c67b6f3b2f018357c5d3125c694
erikslewis <Adelina198527@gmail.com>
2017-01-25 15:13:58 -0500

    121q11newaooo

--- script.js
+++ script.js
@@ -8,9 +8,18 @@ document.getElementById("snap").addEventListener("click", function() {
 	context.drawImage(video, 0, 0, 640, 480);
 });
 
-var dataURL = canvas.toDataURL();
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
 
-document.getElementById('canvasImg').src = dataURL;
+/**
+ * The event handler for the link's onclick event. We give THIS as a
+ * parameter (=the link element), ID of the canvas and a filename.
+*/
+document.getElementById('download').addEventListener('click', function() {
+    downloadCanvas(this, 'canvas', 'test.png');
+}, false);
 
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
